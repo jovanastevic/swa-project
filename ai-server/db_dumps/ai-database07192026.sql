65257d9b901fbbcad7e79bbe0a95700dc40f0cb3
Avoqato2 <mt241087@fhstp.ac.at>
2026-07-19 17:26:36 +0200

    new dbDump

---
+++ ai-server/db_dumps/ai-database07192026.sql
@@ -0,0 +1,225 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 19. Jul 2026 um 17:24
+-- Server-Version: 10.4.32-MariaDB
+-- PHP-Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Datenbank: `ai-database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `category`
+--
+
+INSERT INTO `category` (`category_id`, `name`, `description`) VALUES
+(1, 'Programming', 'This category is for discussing programming problems. So, if you need help or have any questions about a topic, feel free to write a prompt.'),
+(2, 'Writing', 'This category is for discussing writing problems. So, if you need help or have any questions about a topic, feel free to write a prompt.'),
+(3, 'Politics', 'This category is for discussing politics. So, if you need help or have any questions about a topic, feel free to write a prompt.');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `chat_member`
+--
+
+CREATE TABLE `chat_member` (
+  `chat_id` int(11) NOT NULL,
+  `username` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `chat_messages`
+--
+
+CREATE TABLE `chat_messages` (
+  `message_id` int(11) NOT NULL,
+  `chat_id` int(11) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `message` text NOT NULL,
+  `time_stamp` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `chat_room`
+--
+
+CREATE TABLE `chat_room` (
+  `chat_id` int(11) NOT NULL,
+  `prompt_id` int(11) NOT NULL,
+  `time_stamp` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `prompts`
+--
+
+CREATE TABLE `prompts` (
+  `prompt_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` text NOT NULL,
+  `time_stamp` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `user`
+--
+
+CREATE TABLE `user` (
+  `username` varchar(25) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `profile_description` varchar(300) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `user`
+--
+
+INSERT INTO `user` (`username`, `password`, `email`, `profile_description`) VALUES
+('Test', '$2b$12$yuiuAHAj7rVmvuwdvtzg8OAl5pcioe4PGYRWYy6s4wC7SzxmvD3j2', 'dajdj@gmail.com', 'Jovana leck Eier!');
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indizes für die Tabelle `chat_member`
+--
+ALTER TABLE `chat_member`
+  ADD KEY `user` (`username`),
+  ADD KEY `chat_room` (`chat_id`);
+
+--
+-- Indizes für die Tabelle `chat_messages`
+--
+ALTER TABLE `chat_messages`
+  ADD PRIMARY KEY (`message_id`),
+  ADD KEY `chatRoom` (`chat_id`),
+  ADD KEY `user_name` (`username`);
+
+--
+-- Indizes für die Tabelle `chat_room`
+--
+ALTER TABLE `chat_room`
+  ADD PRIMARY KEY (`chat_id`),
+  ADD KEY `prompt_id` (`prompt_id`);
+
+--
+-- Indizes für die Tabelle `prompts`
+--
+ALTER TABLE `prompts`
+  ADD PRIMARY KEY (`prompt_id`),
+  ADD KEY `category` (`category_id`),
+  ADD KEY `username` (`username`);
+
+--
+-- Indizes für die Tabelle `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT für Tabelle `chat_messages`
+--
+ALTER TABLE `chat_messages`
+  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `chat_room`
+--
+ALTER TABLE `chat_room`
+  MODIFY `chat_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `prompts`
+--
+ALTER TABLE `prompts`
+  MODIFY `prompt_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `chat_member`
+--
+ALTER TABLE `chat_member`
+  ADD CONSTRAINT `chat_room` FOREIGN KEY (`chat_id`) REFERENCES `chat_room` (`chat_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
+
+--
+-- Constraints der Tabelle `chat_messages`
+--
+ALTER TABLE `chat_messages`
+  ADD CONSTRAINT `chatRoom` FOREIGN KEY (`chat_id`) REFERENCES `chat_room` (`chat_id`),
+  ADD CONSTRAINT `user_name` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
+
+--
+-- Constraints der Tabelle `chat_room`
+--
+ALTER TABLE `chat_room`
+  ADD CONSTRAINT `prompt_id` FOREIGN KEY (`prompt_id`) REFERENCES `prompts` (`prompt_id`);
+
+--
+-- Constraints der Tabelle `prompts`
+--
+ALTER TABLE `prompts`
+  ADD CONSTRAINT `category` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`),
+  ADD CONSTRAINT `username` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

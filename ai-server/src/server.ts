3d5cce8a950ca14dde455a1e39d385521ff9b752
Jovana Stevic <jovana.stevic31@gmail.com>
2026-08-20 00:39:54 +0200

    changed to server bc websocket didnt work

--- ai-server/src/server.ts
+++ ai-server/src/server.ts
@@ -1,6 +1,6 @@
-import {app} from './index';
+import {app, server} from './index';
 
 // for jest this file is separated from index.ts, so we need to start the server here
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
